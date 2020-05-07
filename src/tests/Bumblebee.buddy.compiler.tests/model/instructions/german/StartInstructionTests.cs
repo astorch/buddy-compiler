e63d8f314fb626460978dde33524d82c62062d0c
Arian Storch <arian.storch@evy-solutions.de>
2020-05-07 14:59:44 +0200

    Applied modern code style

--- src/tests/Bumblebee.buddy.compiler.tests/model/instructions/german/StartInstructionTests.cs
+++ src/tests/Bumblebee.buddy.compiler.tests/model/instructions/german/StartInstructionTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Bumblebee.buddy.compiler.model;
 using Bumblebee.buddy.compiler.model.evaluation;
@@ -9,23 +10,31 @@ namespace Bumblebee.buddy.compiler.tests.model.instructions.german {
     [TestFixture]
     public class StartInstructionTests {
 
-        [Test, TestCaseSource(typeof(TestCaseFactory), "TestCases")]
+        [Test, TestCaseSource(typeof(TestCaseFactory), nameof(TestCaseFactory))]
         public IInstructionEvaluationResult Evaluate(string instruction) {
-            return new InstructionEvaluator().Evaluate(instruction,
+            return new InstructionEvaluator().Evaluate(
+                instruction,
                 new InstructionTranslationInfo(new StartBuddyTranslationInstruction()),
-                new Dictionary<string, IPatternParameter>());
+                new Dictionary<string, IPatternParameter>()
+            );
         }
 
-        class TestCaseFactory {
-            public IEnumerable<TestCaseData> TestCases {
-                get {
-                    yield return new TestCaseData(@"Starte ""C:\EGUB\EGUB.exe"".").Returns(EvaluationResult.Ok);
+        class TestCaseFactory : IEnumerable {
+            /// <inheritdoc />
+            public IEnumerator GetEnumerator() {
+                yield return new TestCaseData(@"Starte ""C:\EGUB\EGUB.exe"".")
+                    .Returns(EvaluationResult.Ok);
 
-                    yield return new TestCaseData((string)null).Throws(typeof(ArgumentNullException));
-                    yield return new TestCaseData(string.Empty).Throws(typeof(ArgumentNullException));
+                yield return new TestCaseData((string)null)
+                    .Throws(typeof(ArgumentNullException));
+                
+                yield return new TestCaseData(string.Empty)
+                    .Throws(typeof(ArgumentNullException));
 
-                    yield return new TestCaseData("Starte {Verwaltung}.").Returns(new _ErrorEvaluationResult("2nd word of instruction does not match expected token 'Parameter reference 'path' (string)'. Word is '{Verwaltung}'"));
-                }
+                yield return new TestCaseData("Starte {Verwaltung}.")
+                    .Returns(
+                        new _ErrorEvaluationResult("2nd word of instruction does not match expected token 'Parameter reference 'path' (string)'. Word is '{Verwaltung}'")
+                    );
             }
         }
     }
