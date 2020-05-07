6f824cd819f83aefe9934d6fdb1e50117da0538a
Arian Storch <arian.storch@evy-solutions.de>
2020-05-07 14:59:02 +0200

    Applied modern code style

--- src/libs/Bumblebee.buddy.compiler/model/IBuddyTranslationInstruction.cs
+++ src/libs/Bumblebee.buddy.compiler/model/IBuddyTranslationInstruction.cs
@@ -1,21 +1,13 @@
 ﻿namespace Bumblebee.buddy.compiler.model {
-    /// <summary>
-    /// Defines a buddy translation instruction.
-    /// </summary>
+    /// <summary> Defines a buddy translation instruction. </summary>
     public interface IBuddyTranslationInstruction {
-        /// <summary>
-        /// Returns id of the directive.
-        /// </summary>
+        /// <summary> Returns id of the directive. </summary>
         string InstructionId { get; }
 
-        /// <summary>
-        /// Returns the pattern of the instruction.
-        /// </summary>
+        /// <summary> Returns the pattern of the instruction. </summary>
         string InstructionPattern { get; }
 
-        /// <summary>
-        /// Returns the resulting TDIL pattern of the directive.
-        /// </summary>
+        /// <summary> Returns the resulting TDIL pattern of the directive. </summary>
         string TdilPattern { get; }
     }
 }
--- src/libs/Bumblebee.buddy.compiler/model/instructions/AbstractBuddyTranslationInstruction.cs
+++ src/libs/Bumblebee.buddy.compiler/model/instructions/AbstractBuddyTranslationInstruction.cs
@@ -1,21 +1,13 @@
 ﻿namespace Bumblebee.buddy.compiler.model.instructions {
-    /// <summary>
-    /// Provides an abstract implementation of <see cref="IBuddyTranslationInstruction"/>.
-    /// </summary>
+    /// <summary> Provides an abstract implementation of <see cref="IBuddyTranslationInstruction"/>. </summary>
     public abstract class AbstractBuddyTranslationInstruction : IBuddyTranslationInstruction {
-        /// <summary>
-        /// Returns id of the action step.
-        /// </summary>
+        /// <inheritdoc />
         public abstract string InstructionId { get; }
-
-        /// <summary>
-        /// Returns the pattern description of the action step.
-        /// </summary>
+        
+        /// <inheritdoc />
         public abstract string InstructionPattern { get; }
-
-        /// <summary>
-        /// Returns the resulting TDIL pattern of the action step.
-        /// </summary>
+        
+        /// <inheritdoc />
         public abstract string TdilPattern { get; }
     }
 }
