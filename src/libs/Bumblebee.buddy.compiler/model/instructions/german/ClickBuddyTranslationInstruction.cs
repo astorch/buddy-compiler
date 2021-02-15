ede1b109fd9e13be8ec03f087d53e4f7a8b535bf
Arian Storch <arian.storch@evy-solutions.de>
2021-02-15 19:28:36 +0100

    Click statement now supports parameter value conjunction

--- src/libs/Bumblebee.buddy.compiler/model/instructions/german/ClickBuddyTranslationInstruction.cs
+++ src/libs/Bumblebee.buddy.compiler/model/instructions/german/ClickBuddyTranslationInstruction.cs
@@ -1,19 +1,15 @@
 ﻿namespace Bumblebee.buddy.compiler.model.instructions.german {
     /// <summary> Implements <see cref="IBuddyTranslationInstruction"/> for the 'click' action. </summary>
     public class ClickBuddyTranslationInstruction : AbstractBuddyTranslationInstruction {
-        /// <summary> Returns id of the action step. </summary>
-        public override string InstructionId {
-            get { return "Klicke"; }
-        }
+        
+        /// <inheritdoc />
+        public override string InstructionId { get; } = "Klicke";
 
-        /// <summary> Returns the pattern description of the action step. </summary>
-        public override string InstructionPattern {
-            get { return "Klicke <{name:alias,type:alias}> {} {name:kind,type:frequence,mandatory:false}"; }
-        }
+        /// <inheritdoc />
+        public override string InstructionPattern { get; } = "Klicke <{name:alias,type:alias}> {name:value,type:param,mandatory:false} {name:kind,type:frequence,mandatory:false}";
 
-        /// <summary> Returns the resulting TDIL pattern of the action step. </summary>
-        public override string TdilPattern {
-            get { return "click(~alias, , ~kind.Value)"; }
-        }
+        /// <inheritdoc />
+        public override string TdilPattern { get; } = "click(~alias, , ~(value&kind.Value))";
+        
     }
 }
