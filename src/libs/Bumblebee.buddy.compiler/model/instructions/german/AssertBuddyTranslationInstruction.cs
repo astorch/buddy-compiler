fd6068ee62c65d07d0fdbe7d1f4694a8854ee88f
Arian Storch <arian.storch@evy-solutions.de>
2021-02-15 18:25:06 +0100

    Doc style

--- src/libs/Bumblebee.buddy.compiler/model/instructions/german/AssertBuddyTranslationInstruction.cs
+++ src/libs/Bumblebee.buddy.compiler/model/instructions/german/AssertBuddyTranslationInstruction.cs
@@ -1,27 +1,21 @@
 ﻿namespace Bumblebee.buddy.compiler.model.instructions.german {
-    /// <summary>
-    /// Implements <see cref="IBuddyTranslationInstruction"/> for the 'assert' action.
-    /// </summary>
+    /// <summary> Implements <see cref="IBuddyTranslationInstruction"/> for the 'assert' action. </summary>
     public class AssertBuddyTranslationInstruction : AbstractBuddyTranslationInstruction {
-        /// <summary>
-        /// Returns id of the action step.
-        /// </summary>
+        
+        /// <inheritdoc />
         public override string InstructionId {
             get { return "Prüfe"; }
         }
-
-        /// <summary>
-        /// Returns the pattern description of the action step.
-        /// </summary>
+        
+        /// <inheritdoc />
         public override string InstructionPattern {
             get { return "Prüfe <{name:alias,type:alias}> {name:value,type:param,mandatory:false} {name:condition,type:condition}"; }
         }
 
-        /// <summary>
-        /// Returns the resulting TDIL pattern of the action step.
-        /// </summary>
+        /// <inheritdoc />
         public override string TdilPattern {
             get { return "assert(~alias, ~condition.Name, ~(value|condition.Value))"; }
         }
+        
     }
 }
